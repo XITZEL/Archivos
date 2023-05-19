828db09a6c525ec43fb4911e11a2d274d7108ee8
XITZEL <124205150+XITZEL@users.noreply.github.com>
2023-05-19 12:02:52 -0700

    Actualizar Program.cs

--- archivos/Program.cs
+++ archivos/Program.cs
@@ -1,80 +1,76 @@
-﻿class Product
-{
-    public string Code {get;set;}
-    public string Description{get;set;}
-    public Decimal Price{get;set;}
+class Product 
+ { 
+     public string Code {get;set;} 
+     public string Description{get;set;} 
+     public Decimal Price{get;set;} 
+  
+  
+     public Product(string c, string d , decimal p) 
+     { 
+         Code= c; 
+         Description = d; 
+         Price = p; 
+     } 
+  
+       
+ } 
+ class ProductsDB 
+ { 
+    public static void SaveProducts(List<Product>products) 
+     { 
+         StreamWriter textOut= 
+             new StreamWriter( 
+                 new FileStream("productos.txt",FileMode.Create,FileAccess.Write)); 
+                     //El orden si importa va en el orden del Constructor 
+                 foreach(var product in products) 
+                 { 
+                     textOut.Write(product.Code + "|"); 
+                     textOut.Write(product.Description + "|"); 
+                     textOut.WriteLine(product.Price); 
+                 } 
+                 textOut.Close(); 
+                 
+     }
+   public static List<Product> GetProducts()
+     {
+        StreamReader textIn = new StreamReader(
+            new FileStream("productos.txt", FileMode.Open, FileAccess.Read));
 
+            List<Product> products = new List<Product>();
+            while (textIn.Peek() != -1)
+            {
+                string row = textIn.ReadLine();
+                string[] columns = row.Split("|");
+                Product product = new Product(columns[0], columns[1], Convert.ToDecimal(columns[2]));
+                products.Add(product);
+            }
+            textIn.Close();
+           return products;
 
-    public Product(string c, string d , decimal p)
-    {
-        Code= c;
-        Description = d;
-        Price = p;
-    }
+     }
+ } 
+ 
+ internal class Program 
+ { 
+     private static void Main(string[] args) 
+     { 
+        //Lista de Productos 
+        List<Product>products= new List<Product>(); 
+        //1er Producto 
+        products.Add(new Product("111", "Lapiz", 20)); 
+        products.Add(new Product("112", "Coca", 16.50m));
+        //Guardamos nuestra lista en un archivo 
+        ProductsDB.SaveProducts(products);
 
-     
-}
-class ProductsDB
-{
-   public static void SaveProducts(List<Product>productos)
-    {
-        StreamWriter textOut=
-            new StreamWriter(
-                new FileStream("productos.txt",FileMode.Create,FileAccess.Write));
-                    //El orden si importa va en el orden del Constructor
-                foreach(var product in productos)
-                {
-                    textOut.Write(product.Code + "|");
-                    textOut.Write(product.Description + "|");
-                    textOut.WriteLine(product.Price);
-                }
-                textOut.Close();
-    }
-
-public static  List<Product> GetProducts()
-{
-    StreamReader textln=
-    new StreamReader(
-        new FileStream("productos.txt",FileMode.Open,FileAccess.Read));
-        List<Product>Products= new List<Product>();
-        while (textln.Peek()!=-1)
+         List<Product> pds = new ();
+       pds = ProductsDB.GetProducts();
+        foreach(var p in pds)
         {
-            string row = textln.ReadLine();
-            string[] columns = ReaderWriterLock.Split("|");
-            Product Product = new Product (columns[0],columns[1],Convert.ToDecimal(columns[2]));
-            ProductsDB.Add(Product);            
-        }
-        textln.Close();
-        return Product();
-}
-
-
-}
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-       //Lista de Productos
-       List<Product>products= new List<Product>();
-       //1er Producto
-       products.Add(new Product("111", "Lapiz", 20));
-       products.Add(new Product("112", "Sacapuntas", 78));
-       //Guardamos nuestra lista en un archivo
-       ProductsDB.SaveProducts(products); 
-
-
-
-       
-       //Leemos nuestros productos
-       List<Product> pds = new();
-       pds=ProductsDB.GetProducts();
-       foreach(Product p in pds)
-       {
-        Console.WriteLine(p.Price);
-       }
-       
-
-
-       
-    }
-}
+          if (p.Price > 15m)
+          {
+               Console.WriteLine(p.Price);
+          }
+        } 
+     }
+     
+ }
